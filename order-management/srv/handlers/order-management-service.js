72f6b9b036632eb8b99c2e9f53aef83ecbe5b4c2
Vũ Trường <maivotruongvu.04@gmail.com>
2025-02-21 05:11:21 +0000

    test process CI/CD

--- order-management/srv/handlers/order-management-service.js
+++ order-management/srv/handlers/order-management-service.js
@@ -6,6 +6,8 @@ const cds = require('@sap/cds')
 module.exports = cds.service.impl(async function () {
     this.on('READ', 'ORDER', async (req, next) => {
         console.log("on read order");
+        // add new
+        console.log("Test build CI/CD for CAP project");
         return next()
     })
 });
